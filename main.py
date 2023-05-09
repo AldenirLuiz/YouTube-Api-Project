3751fddffea3292bdfdfb5b01c6c37fca3c676b9
Aldenir Luiz <aldenir.sky@gmail.com>
2023-05-09 08:47:49 -0300

    melhoria nas thumbnails, cache de tumbnails adicionado

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from MyYouTube_API import MyYoutube
 from tkinter import Tk,  Frame, Label, Canvas, Button, Scrollbar, VERTICAL
 import sys 
@@ -17,7 +18,7 @@ class MyView:
         self.myFrame02.grid_rowconfigure(0, weight=1)
         self.myFrame02.grid_columnconfigure(0, weight=1)
         self.myFrame02.grid_propagate(False)
-        self.canvas = Canvas(self.myFrame02, )
+        self.canvas = Canvas(self.myFrame02)
         self.canvas.grid(row=0, column=0, sticky="news")
         self.scroll_bar = Scrollbar(self.myFrame02, orient=VERTICAL, command = self.canvas.yview)
         self.scroll_bar.grid(row=0, column=1, sticky='ns')
@@ -54,16 +55,16 @@ class MyView:
             video_id=item["snippet"]["resourceId"]["videoId"]
 
             self.widgets.update({
-                f"image{count}": Label(tempFrame, text=f"image{count}", image=self.not_found),
-                f"text{count}": Label( tempFrame, text=f'{item["snippet"]["title"]}'),
-                f"descT{count}": Label(tempFrame, text=f'Canal: {item["snippet"]["channelTitle"]}'),
-                f"descD{count}": Label(tempFrame, text=f'Data: {item["snippet"]["publishedAt"]}'),
-                f"descId{count}": Label(tempFrame, text=f'vId: {item["snippet"]["resourceId"]["videoId"]}'),
-                f"button{count}": Button(tempFrame, text="Download", relief="flat", bg="cyan",
+                f"image{count}": Label(tempFrame, text=f"image{count}", image=self.not_found, anchor="sw"),
+                f"text{count}": Label( tempFrame, text=f'{item["snippet"]["title"]}', anchor="sw"),
+                f"descT{count}": Label(tempFrame, text=f'Canal: {item["snippet"]["channelTitle"]}', anchor="sw"),
+                f"descD{count}": Label(tempFrame, text=f'Data: {item["snippet"]["publishedAt"]}', anchor="sw"),
+                f"descId{count}": Label(tempFrame, text=f'vId: {item["snippet"]["resourceId"]["videoId"]}', anchor="sw"),
+                f"button{count}": Button(tempFrame, text="Download", relief="flat", bg="cyan", anchor="center",
                     command=lambda id=video_id, btt=count: threading.Thread(target=self.download, args=(id, btt)).start())}
             )
             tempFrame.pack(expand=True, fill="x",
-                ipadx=4, ipady=4, padx=2, pady=2)
+                ipadx=4, ipady=4, padx=2, pady=2, anchor="center")
             
             try:
                 target = self.widgets[f"image{count}"]
@@ -74,34 +75,42 @@ class MyView:
             except KeyError:
                 continue
             
-            
         viewFrame.pack(expand=True, fill="both",
-            ipadx=4, ipady=4, padx=2, pady=2)
+            ipadx=4, ipady=4, padx=2, pady=2, anchor="center")
         self.myFrame02.config(width=600,height=700)
 
     def show_view(self):
         for key, widget in self.widgets.items():
             target:Label|Button = widget
             if "image" in key:
-                target.pack(side="left", expand=False, fill="x")
+                target.pack(side="left", expand=False, ipadx=1, ipady=1, padx=1, pady=1)
                 self.internal_frame.update_idletasks()
             elif "text" in key:
-                target.pack(side="top", expand=False, fill="x")
+                target.pack(expand=False, fill="x", ipadx=1, ipady=1, padx=1, pady=1)
             elif "desc" in key:
-                target.pack(side="top", expand=False, fill="x")
+                target.pack(expand=False, fill="x", ipadx=1, ipady=1, padx=1, pady=1)
             elif "button" in key:
-                target.pack(expand=False, fill="x", anchor="e")
+                target.pack(expand=False, fill="x")
 
     def get_thumb(self, target:Label, url:str, id):
         master=target.master
-        thumb = GetThumb(url, id=id)
-        thumb.start()
-        thumb.join()
-        img = ImageTk.PhotoImage(Image.open(thumb.fileName))
+
+        if os.path.exists(f"{os.path.dirname(__file__)}\\thumbs\\thumb{id}.jpg"):
+            self.set_image(target, id)
+        else:
+            thumb = GetThumb(url, id=id)
+            thumb.start()
+            thumb.join()
+            self.set_image(target, id)
+            
+    def set_image(self, target:Label, id):
+        master=target.master
+        img = ImageTk.PhotoImage(Image.open(f"thumbs/thumb{id}.jpg"))
         self.thumbs[str(id)] = img
-        target.configure(image=self.thumbs[str(id)], background="red", relief="sunken", bd=4)
+        target.configure(image=self.thumbs[str(id)], background="red", relief="sunken")
         master.update_idletasks()
         target.update()
+        
 
 
     def download(self, video_id, button):
@@ -137,6 +146,7 @@ class View:
         self.primary_label = Label(self.primary_frame, text="Carregando...")
         self.primary_frame.pack()
         self.primary_label.pack()
+        self.primary_frame.update_idletasks()
         # Inicializado o Id da playlist
         self.playlist = playlist_key
 
